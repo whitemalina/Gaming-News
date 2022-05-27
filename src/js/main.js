557c17e92253e108723065aff3849a3a2d3b1f4d
WhiteMalina <danil.bayramov.03@gmail.com>
2022-05-28 03:58:49 +0500

    123

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 const { createApp } = Vue;
+var notyf = new Notyf();
 const options = {
   method: "GET",
   headers: {
-    "Access-Control-Allow-Origin": "*",
     "X-RapidAPI-Host": "gaming-news.p.rapidapi.com",
     "X-RapidAPI-Key": "bdfbeeb0b1msh85778f6f18922a0p137b2bjsn943b41484846",
   },
@@ -16,12 +16,19 @@ createApp({
     };
   },
   async mounted() {
-    await fetch("//gaming-news.p.rapidapi.com/news", options)
+    await fetch("https://gaming-news.p.rapidapi.com/news", options)
       .then((response) => response.json())
       .then((response) => (this.news = response))
       .catch((err) => console.error(err));
     this.start = 20;
     console.log("News fetched");
+    for (let i = 0; i < this.news.length; i++) {
+      let string
+      let num = Math.floor(Math.random() * 5);
+      string = 'background: url("../src/img/' + num + '.jpg") center / cover no-repeat  !important'
+      this.news[i].bg = string
+    }
+    
   },
   computed: {
     newsList() {
@@ -29,6 +36,12 @@ createApp({
     },
   },
   methods: {
+    openUrl(url) {
+      window.open(url, "_blank");
+    },
+    toast(message) {
+      notyf.success(message);
+    },
     getNews() {
       const options = {
         method: "GET",
