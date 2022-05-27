a7c92cf3799052ced61c8cf46b2877b1df29e444
WhiteMalina <danil.bayramov.03@gmail.com>
2022-05-27 05:13:12 +0500

    0.0.1

---
+++ src/js/main.js
@@ -0,0 +1,57 @@
+const { createApp } = Vue;
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "gaming-news.p.rapidapi.com",
+    "X-RapidAPI-Key": "bdfbeeb0b1msh85778f6f18922a0p137b2bjsn943b41484846",
+  },
+};
+createApp({git init
+  data() {
+    return {
+      news: [],
+      step: 3,
+    };
+  },
+  async mounted() {
+    await fetch("https://gaming-news.p.rapidapi.com/news", options)
+      .then((response) => response.json())
+      .then((response) => (this.news = response))
+      .catch((err) => console.error(err));
+    console.log("News fetched");
+  },
+  computed: {
+    newsList() {
+      return this.news.slice(0, this.step);
+    },
+  },
+  methods: {
+    getNews() {
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Host": "gaming-news.p.rapidapi.com",
+          "X-RapidAPI-Key":
+            "bdfbeeb0b1msh85778f6f18922a0p137b2bjsn943b41484846",
+        },
+      };
+
+      fetch("https://gaming-news.p.rapidapi.com/news", options)
+        .then((response) => response.json())
+        .then((response) => console.log(response))
+        .catch((err) => console.error(err));
+    },
+    pushStep() {
+      this.step += 4;
+      console.log("News added");
+    },
+    refresh() {
+      this.step = 0;
+      setTimeout(() => {
+        this.step = 3;
+      }, 1000);
+
+      console.log("Refreshed");
+    },
+  },
+}).mount("#app");
